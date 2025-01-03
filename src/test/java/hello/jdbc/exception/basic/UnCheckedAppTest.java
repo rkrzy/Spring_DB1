e6fee6c73630d18fb34f26d574170f629f052c76
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-03 12:10:58 +0900

    Title : 예외 포함과 스택 트레이스 - 예외를 전환해서 출력할때는 기존에 있는 예외중 어떤 것들 중에서 발생했는지 출력을 해줘야 한다. - 개발자는 로그확인을 많이 하게 된다.

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -15,6 +17,16 @@ public class UnCheckedAppTest {
                 .isInstanceOf(RuntimeSQLException.class);
     }
 
+    @Test
+    void printEx(){
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        }catch (Exception e){
+            //e.printStackTrace();
+            log.info("ex", e);
+        }
+    }
     static class Controller{
         Service service = new Service();
 
@@ -55,9 +67,7 @@ public class UnCheckedAppTest {
         }
     }
     static class RuntimeSQLException extends RuntimeException{
-        public RuntimeSQLException(){
 
-        }
         public RuntimeSQLException(Throwable cause){
             super(cause);
         }

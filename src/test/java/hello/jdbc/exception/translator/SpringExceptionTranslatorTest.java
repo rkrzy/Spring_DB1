c87d2f6e4bfd4a97108bd2a7b101cca9a74522f3
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-01-06 21:43:23 +0900

    Title : 스프링 예외 추상화 이해 - 스프링의 예외 변환기를 통해서 SQLException의 ErrorCode를 적절한 예외값으로 반환해줄 수 있다. - 예외의 최고 상위는 DataAccessException이다

--- src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
+++ src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -1,2 +1,68 @@
-package hello.jdbc.exception.translator;public class SpringExceptionTranslatorTest {
+package hello.jdbc.exception.translator;
+
+import hello.jdbc.connection.ConnectionConst;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class SpringExceptionTranslatorTest {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringExceptionTranslatorTest.class);
+    DataSource dataSource;
+
+    @BeforeEach
+    void init(){
+        dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+
+    @Test
+    void sqlExceptionErrorCode(){
+        String sql = "select bad grammer";
+
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeQuery();
+        }catch (SQLException e){
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+            int errorCode = e.getErrorCode();
+            log.info("errorCode={}",errorCode);
+            log.info("error", e);
+        }
+    }
+    @Test
+    void exceptionTranslator() {
+        String sql = "select bad grammer";
+
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.executeQuery();
+        }catch (SQLException e){
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+
+            //org.springframework.jdbc.sql-error-codes.xml
+            SQLErrorCodeSQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+
+            //BadSqlGrammarException
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx", resultEx);
+            assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
+        }
+    }
 }
